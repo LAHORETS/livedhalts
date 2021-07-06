c8451a9ecf664cd3ffd2d85dea59ee2a55db4200
LAHORETS <77092775+LAHORETS@users.noreply.github.com>
2021-07-06 13:05:17 +0500

    Add files via upload

---
+++ document_validation/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError, UserError
+
+class DocumentAttachmentCustomValidate(models.Model):
+    _inherit = "ir.attachment"
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        if 'name' in vals_list[0]:
+            prev_files = self.env['documents.document'].search([]).mapped('name')
+            if vals_list[0]['name'] in prev_files:
+                raise UserError(_('file already existed'))
+            else:
+                res = super(DocumentAttachmentCustomValidate, self).create(vals_list)
+                return res
+
