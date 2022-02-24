c6b672a1b7d3ef59451f76b8866689df4a9dcec7
LAHORETS <77092775+LAHORETS@users.noreply.github.com>
2022-02-24 16:22:55 +0500

    Add files via upload

--- payslip_partner/models/models.py
+++ payslip_partner/models/models.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import models, api, fields, models, _
+from odoo.tools import float_compare, float_is_zero
+from odoo.exceptions import UserError, ValidationError
 
 
 class HrPayslipInh(models.Model):
     _inherit = 'hr.payslip'
 
-    def _action_create_account_move(self):
-        record = super(HrPayslipInh, self)._action_create_account_move()
-        for rec in self:
-            for line in rec.move_id.line_ids:
-                line.partner_id = rec.employee_id.address_home_id.id
-        return record
+    def _prepare_line_values(self, line, account_id, date, debit, credit):
+        return {
+            'name': line.name,
+            'partner_id': line.slip_id.employee_id.address_home_id.id if line.slip_id.employee_id.address_home_id else line.partner_id.id,
+            'account_id': account_id,
+            'journal_id': line.slip_id.struct_id.journal_id.id,
+            'date': date,
+            'debit': debit,
+            'credit': credit,
+            'analytic_account_id': line.salary_rule_id.analytic_account_id.id or line.slip_id.contract_id.analytic_account_id.id,
+        }
